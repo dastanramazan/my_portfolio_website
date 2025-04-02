1f191c563cb929ee6c8a6a89bf7833a740e33e80
Dastan Ramazan <dastanramazan@gmail.com>
2025-04-02 14:48:43 -0500

    Revised the responsiveness and other things

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -11,21 +11,42 @@ const data = [
     image: Image1,
     title: "UI/UX design",
     description:
-      "User Experience (UX) and User Interface (UI) design are at the heart of creating products that resonate with users. I offer comprehensive UX/UI design services that blend creativity with user-centric strategies, ensuring your digital solutions are not only visually stunning but also highly functional and intuitive.",
+      "I offer comprehensive UX/UI design services that blend creativity with user-centric strategies, ensuring your digital solutions are not only visually stunning but also highly functional and intuitive.",
   },
   {
     id: 2,
     image: Image2,
     title: "Web Development",
     description:
-      "Are you looking to establish a strong online presence? I specialize in creating modern, user-friendly websites that engage your audience and achieve your business goals. Whether it's a portfolio, e-commerce platform, or informational site, I'll bring your vision to life with clean code and stunning design.",
+      "Are you looking to establish a strong online presence? I specialize in creating modern, user-friendly websites that engage your audience and achieve your business goals.",
   },
   {
     id: 3,
     image: Image3,
     title: "Animation and Data Visualization",
     description:
-      "Animation:Elevate digital experiences with captivating motion. Our animation services transform ideas into interactive user journeys that make a lasting impact. Data Visualization: Unlock insights from data with compelling visuals. Our data visualization services present complex information in an understandable format, empowering informed decisions.",
+      "Animation:Elevate digital experiences with captivating motion. Our animation services transform ideas into interactive user journeys that make a lasting impact.",
+  },
+  {
+    id: 4,
+    image: Image3,
+    title: "Animation and Data Visualization",
+    description:
+      "Animation:Elevate digital experiences with captivating motion. Our animation services transform ideas into interactive user journeys that make a lasting impact.",
+  },
+  {
+    id: 5,
+    image: Image3,
+    title: "Animation and Data Visualization",
+    description:
+      "Animation:Elevate digital experiences with captivating motion. Our animation services transform ideas into interactive user journeys that make a lasting impact.",
+  },
+  {
+    id: 6,
+    image: Image3,
+    title: "Animation and Data Visualization",
+    description:
+      "Animation:Elevate digital experiences with captivating motion. Our animation services transform ideas into interactive user journeys that make a lasting impact.",
   },
 ];
 
@@ -41,6 +62,7 @@ const Services = () => {
             <h3 className="services__title">{title}</h3>
             <p className="services__description">{description}</p>
           </div>
+          
         )
       })}
 
